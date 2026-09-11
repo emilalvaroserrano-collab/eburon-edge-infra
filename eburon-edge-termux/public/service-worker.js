fc58b16dac1630401036ac80c4f85a646bbb01b4
emilalvaroserrano-collab <emilalvaroserrano@gmail.com>
2026-09-11 15:03:50 +0800

    release(pwa): rotate cache for v0.4.0

--- eburon-edge-termux/public/service-worker.js
+++ eburon-edge-termux/public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='eburon-edge-v0.3.4-ort-file-factory';
+const CACHE='eburon-edge-v0.4.0-deno-runtime';
 const CORE=['/','/index.html','/translate.html','/settings.html','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
